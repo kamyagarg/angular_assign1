66de387dbe42f0ead66d29575ed83eae30b34c97
kamya <kamya@piktorlabs.com>
2021-01-09 03:16:30 +0530

    home component ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,19 +9,33 @@ import { HttpService } from '../http.service';
 })
 export class HomeComponent implements OnInit {
 
-  brews: Object = {};
+  brews: Object ={};
+  new_brews: Object ={};
 
   constructor(private _http: HttpService) { }
 
   ngOnInit() {
     this._http.getBeer().subscribe(data => {
-      this.brews =data;
+      this.brews = data;
       console.log(this.brews);
+      console.log("data",data[0].brewery_type);
+      for (let i in this.brews){
+        // console.log("items", i);
+        // console.log("type ofitems", typeof(i));
+        if(data[i].brewery_type == "brewpub"){
+          console.log("found");
+          console.log(data[i]);
+          this.new_brews = data[i];
+          console.log("this new brews:",typeof(this.new_brews));
+        }
+      }
+
+      // console.log("data",typeof(data));
     });
   }
 
-  showIng(){
+  // showIng(){
 
-  }
+  // }
 
 }
