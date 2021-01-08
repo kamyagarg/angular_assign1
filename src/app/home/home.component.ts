9e7db252ec3b13f48a613ddb83c32fbf2b3f37f4
kamya <kamya@piktorlabs.com>
2021-01-09 00:32:50 +0530

    angular project

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  brews: Object = {};
+
+  constructor(private _http: HttpService) { }
+
+  ngOnInit() {
+    this._http.getBeer().subscribe(data => {
+      this.brews =data;
+      console.log(this.brews);
+    });
+  }
+
+  showIng(){
+
+  }
+
+}
